a225354037601ef40d9f7c713ce86511d87496ad
Angel N <nwankwo.angie@gmail.com>
2026-09-10 00:16:39 +0100

    Setting Up React Project For Blog

---
+++ src/Navbar.js
@@ -0,0 +1,23 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>The Payment Blog</h1>
+            <div className="links">
+                <ul className="nav-menu">
+                    <li className="nav-item">
+                        <a href="/" className="nav-link">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/about" className="nav-link">About</a>
+                    </li>
+                    <li className="nav-item">
+                        <a href="/create" className="nav-link">New Blog</a>
+                    </li>
+                </ul>      
+            </div>
+           
+        </nav>
+    );
+}
+ 
+export default Navbar;
